e36659d0271ffa0ad0b099af0fe7fb89ce98b2d0
indu sharma <indusharma.er@gmail.com>
2022-09-11 11:08:31 +0530

    first commit

---
+++ src/test/java/TestCases/HomeTest.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class HomeTest extends BaseClass {
+	
+	@Test
+	
+	public void Test1() {
+		System.out.println("Inside Home Test");
+	}
+
+}
